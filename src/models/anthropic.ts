454709a0208beaaf58cf042db5238ff800cc4636
Marcin Krawczyk <marckraw@icloud.com>
2024-07-13 15:34:52 +0200

    fix: trigger

--- src/models/anthropic.ts
+++ src/models/anthropic.ts
@@ -11,8 +11,6 @@ export class AnthropicModel implements AIModel {
       throwErrorIfBrowser("AnthropicModel");
     }
 
-    console.log("Outrageous!");
-
     this.api = axios.create({
       baseURL: this.baseUrl,
       headers: {
